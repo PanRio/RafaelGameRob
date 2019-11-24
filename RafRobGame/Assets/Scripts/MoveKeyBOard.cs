58abd210e9b87447218fd13bf5a77163bf04f902
Roberto <roberto.laczak@gmail.com>
2019-11-24 20:35:36 +0100

    Masz

---
+++ RafRobGame/Assets/Scripts/MoveKeyBOard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveKeyBOard : MonoBehaviour
+{
+    public float speed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"))*speed);
+
+
+    }
+}
