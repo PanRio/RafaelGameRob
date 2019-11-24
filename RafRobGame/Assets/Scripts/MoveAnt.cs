994f7e7712dd680ed5d23e88620b625ffc79e7e1
Rafał <rafalmiskiewicz9@gmail.com>
2019-11-24 22:55:16 +0100

    Ant move

--- RafRobGame/Assets/Scripts/MoveAnt.cs
+++ RafRobGame/Assets/Scripts/MoveAnt.cs
@@ -5,9 +5,64 @@ using UnityEngine;
 public class MoveAnt : MonoBehaviour
 {
 
-    public float speed = 0.001f;
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 0.01f;
+    private Vector3 move;
+    public Transform player;
+    public int motionX = 2,	motionZ = 2;
+
+    private void calcualte()
+    {
+
+        if (transform.position.z - motionZ > player.position.z)
+        {
+            if (transform.position.x  - motionX > player.position.x)
+            {
+                Debug.Log("góra_lewo");
+                if (motionX > 0)
+                    motionX *= -1;
+                if (motionZ > 0)
+                    motionZ *= -1;
+            }
+            else
+            {
+                Debug.Log("góra_prawo");
+                if (motionX < 0)
+                    motionX *= -1;
+                if (motionZ > 0)
+                    motionZ *= -1;
+
+            }
+        }
+        else
+        {
+            if (transform.position.x  - motionX > player.position.x)
+            {
+                Debug.Log("dół_lewo");
+                if (motionX > 0)
+                    motionX *= -1;
+                if (motionZ < 0)
+                    motionZ *= -1;
+
+            }
+            else
+            {
+                Debug.Log("dół_prawo");
+                if (motionX < 0)
+                    motionX *= -1;
+                if (motionZ < 0)
+                    motionZ *= -1;
+
+            }
+        }
+
+    }
+
+    private void calcualte2()
+    {
+
+    }
+        // Start is called before the first frame update
+        void Start()
     {
         
     }
@@ -15,6 +70,14 @@ public class MoveAnt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(1, 0, 1) * speed);
+        calcualte();
+
+        move.x = motionX*speed;
+        move.y = 0;
+        move.z = motionZ * speed;
+
+
+        //Debug.Log(move);
+        transform.Translate(move);
     }
 }
--- RafRobGame/Assets/Scripts/MoveKeyBOard.cs
+++ RafRobGame/Assets/Scripts/MoveKeyBOard.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MoveKeyBOard : MonoBehaviour
 {
-    public float speed = 100f;
+    public float speed = 1f;
+    public Transform player;
 
     // Start is called before the first frame update
     void Start()
